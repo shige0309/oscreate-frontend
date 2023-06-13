70c17c422e89c313c9bb78d6dde122e868e520ec
shige0309 <qqa27yed@chic.ocn.ne.jp>
2023-06-13 13:40:49 +0900

    WORK追加、SubContent追加

--- src/components/SubContent/index.tsx
+++ src/components/SubContent/index.tsx
@@ -1,7 +1,8 @@
 import React, { ReactNode } from 'react'
+import './SubContent.css';
 
 export const SubContent = ({children} : {children: ReactNode}) => {
   return (
-    <div>{children}</div>
+    <div className="sub-content">{children}</div>
   )
 }
--- src/pages/Work/index.tsx
+++ src/pages/Work/index.tsx
@@ -2,15 +2,22 @@ import { Contact } from "../../components/Contact"
 import { Content } from "../../components/Content"
 import { MainVisual } from "../../components/MainVisual"
 import { SubContent } from "../../components/SubContent"
+import './Work.css'
 
 export const Work = () => {
   return (
     <main>
       <MainVisual image={"work/mv.jpg"}/>
       <Content>
+        <div className="work">
+          <p className="work-title-en area-400">WORk</p>
+          <p className="work-title-sub area-400">WEB SITE</p>
+          <h1 className="work-title-title">キャトル株式会社様</h1>
+        </div>
         <SubContent>
-          <Contact />
+          <p><img src="/work/detail-img.jpg" alt="" /></p>
         </SubContent>
+        <Contact />
       </Content>
     </main>
   )
